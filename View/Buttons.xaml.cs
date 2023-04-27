e83eefa002ef9f9d0487f47656ce2f83276e7d36
KLizakk <116660589+KLizakk@users.noreply.github.com>
2023-04-27 21:17:23 +0200

    Dodaj pliki projektów.

---
+++ View/Buttons.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Players.Model.PlayersModel;
+
+namespace Players.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Buttons.xaml
+    /// </summary>
+    public partial class Buttons : DataGridd
+    {
+        public Buttons()
+        {
+            InitializeComponent();
+        }
+
+        private async void SzukajZ_Click(object sender, RoutedEventArgs e)
+        {
+        
+        }
+
+
+
+
+
+
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SzukajK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PodajIlosc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ViewModel/Funkcje.cs
@@ -0,0 +1,68 @@
+﻿using Players.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static Players.Model.PlayersModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
+
+namespace Players
+{
+    public class Funkcje : MainWindow 
+    {
+        public async void WczytajDane()
+        {
+            string WprowadzoneImie = WprowadzanieImienia.Text;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://api-nba-v1.p.rapidapi.com/players?search={WprowadzoneImie}"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "82e608ca0amshfb8be84d382c693p152ebejsn0ed3121d87c8" },
+        { "X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+
+                response.EnsureSuccessStatusCode();
+
+
+
+                var body = await response.Content.ReadAsStringAsync();
+                if (string.IsNullOrEmpty(body) == false)
+                {
+                   Testowa.Items.Clear();
+
+                    var Wczytywanie = JsonConvert.DeserializeObject<Root>(body);
+
+                    int LiczbaZawodników = Wczytywanie.response.Count();
+
+                    // Dodajemy zawodników do tabeli
+                    for (int i = 0; i < LiczbaZawodników; i++)
+                    {
+                        //tworzenie Zawodnika lepiej 
+                        ZAWODNIK z = new ZAWODNIK();
+                        z.firstname = Wczytywanie.response[i].lastname;
+                        z.lastname = Wczytywanie.response[i].firstname;
+                        z.nba = Wczytywanie.response[i].nba.start.ToString();
+                        z.weight = Wczytywanie.response[i].weight.kilograms;
+                        z.height = Wczytywanie.response[i].height.meters;
+                        z.id = Wczytywanie.response[i].id;
+                        z.college = Wczytywanie.response[i].college;
+                        z.affiliation = Wczytywanie.response[i].affiliation;
+
+                        //dodanie z do DataGridu
+                        Testowa.Items.Add(z);
+                    }
+                }
+    }
+    }
+}
